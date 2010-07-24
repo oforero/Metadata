fd5bd3888c0a75a0022f782908aed6b7cdd55ff3
Oscar Forero <omforero@googlemail.com>
2010-07-25 01:44:09 +0200

    Directory reorg

---
+++ compiler-plugin/src/it/test-plugin-project/src/main/scala/MetadataIT.scala
@@ -0,0 +1,10 @@
+package uk.ac.liv.oforero.metadata_it
+
+object MetadataIT {
+  import uk.ac.liv.oforero.metadata.plain.Metadata._
+
+  def doStuff {
+    assert( 5 == (5 addMeta ("serializable" → true)))
+    assert( (5 addMeta ("serializable" → true)) == 5)
+  }
+}
